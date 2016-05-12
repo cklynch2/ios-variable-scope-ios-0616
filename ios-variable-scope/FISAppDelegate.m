bceab168958d9d70d10aa765d95911634bf3d9ee
cklynch2 <cklynch2@gmail.com>
2016-05-12 16:26:23 -0400

    Variable scope all day erry day

--- ios-variable-scope/FISAppDelegate.m
+++ ios-variable-scope/FISAppDelegate.m
@@ -19,10 +19,31 @@
     return YES;
 }
 
-/*
- 
- * Write your method definitions here.
- 
- */
+-(NSMutableArray *)arrayByAddingString:(NSString *)string toArray:(NSMutableArray *)array
+{
+    NSMutableArray *arrayAddString = [array mutableCopy];
+    [arrayAddString addObject:string];
+    return arrayAddString;
+}
+
+-(NSUInteger)countOfUppercaseStringsInArray:(NSArray *)array
+{
+//        NSMutableArray *uppercaseStrings = [[NSMutableArray alloc]init];
+//        NSUInteger countOfUppercaseStrings = [uppercaseStrings count];
+    NSUInteger countOfUppercaseStrings = 0;
+    for (NSUInteger i = 0; i < [array count]; i++) {
+        NSString *string = array[i];
+        NSString *uppercaseString = [string uppercaseString];
+        if ([string isEqualToString:uppercaseString]) {
+            countOfUppercaseStrings += 1;
+        }
+    }
+    return countOfUppercaseStrings;
+}
+
+-(void)removeAllObjectsFromArray:(NSMutableArray *)array
+{
+    [array removeAllObjects];
+}
 
 @end
